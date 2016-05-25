9f50f414fa879c007451c8c25c86e5b16fdc987f
Leejay Schmidt <leejayschmidt@Leejays-MacBook-Pro.local>
2016-05-25 16:37:40 -0600

    Added NERDTree to installation, added bolding of text

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,71 +1,87 @@
 #!/bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+uline=$(tput smul)
+
 # install pathogen
 if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
-    echo "[vim_setup] Installing pathogen"
-    echo "[vim_setup] Making the necessary directories in vim parent directory"
+    echo "${bold}[vim_setup]${normal} Installing pathogen"
+    echo "${bold}[vim_setup]${normal} Making the necessary directories in vim parent directory"
     mkdir -p ~/.vim/autoload ~/.vim/bundle &&\
-    echo "[vim_setup] Downloading and copying pathogen"
+    echo "${bold}[vim_setup]${normal} Downloading and copying pathogen"
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-    echo "[vim_setup] Completed installing pathogen"
+    echo "${bold}[vim_setup]${normal} Completed installing pathogen"
 else
-    echo "[vim_setup] Skipping pathogen install -- already installed"
+    echo "${bold}[vim_setup]${normal} Skipping pathogen install -- already installed"
+fi
+
+# install nerdtree
+if [ ! -d ~/.vim/bundle/nerdtree ]; then
+    echo "${bold}[vim_setup]${normal} Installing NERDTree"
+    pushd ~/.vim/bundle
+    echo "${bold}[vim_setup]${normal} Cloning NERDTree repository"
+    git clone https://github.com/scrooloose/nerdtree.git
+    popd
+    echo "${bold}[vim_setup]${normal} Completed installing NERDTree"
+else
+    echo "${bold}[vim_setup]${normal} Skipping NERDTree install -- already installed"
 fi
 
 # install clang_complete
 if [ ! -f ~/.vim/plugin/clang_complete.vim ]; then
-    echo "[vim_setup] Installing clang_complete"
-    echo "[vim_setup] Creating temp folder to clone clang_complete into"
+    echo "${bold}[vim_setup]${normal} Installing clang_complete"
+    echo "${bold}[vim_setup]${normal} Creating temp folder to clone clang_complete into"
     pushd ~/Documents
     mkdir vim_temp
     pushd vim_temp
-    echo "[vim_setup] Cloning clang_complete repository"
+    echo "${bold}[vim_setup]${normal} Cloning clang_complete repository"
     git clone https://github.com/Rip-Rip/clang_complete.git
     pushd clang_complete
-    echo "[vim_setup] Building the vimball"
+    echo "${bold}[vim_setup]${normal} Building the vimball"
     make
-    echo "[vim_setup] Unballing the vimball"
+    echo "${bold}[vim_setup]${normal} Unballing the vimball"
     if [ -f clang_complete.vmb ]; then
         vim clang_complete.vmb -c 'so %' -c 'q'
     elif [ -f clang_complete.vmb.vba ]; then
         vim clang_complete.vmb.vba -c 'so %' -c 'q'
     fi
     popd
-    echo "[vim_setup] Removing the temp folder"
+    echo "${bold}[vim_setup]${normal} Removing the temp folder"
     rm -rf clang_complete
     popd
     rm -rf vim_temp
     popd
-    echo "[vim_setup] Completed installing clang_complete"
+    echo "${bold}[vim_setup]${normal} Completed installing clang_complete"
 else
-    echo "[vim_setup] Skipping clang_complete install -- already installed"
+    echo "${bold}[vim_setup]${normal} Skipping clang_complete install -- already installed"
 fi
 
 # install pymode
 if [ ! -d ~/.vim/bundle/python-mode ]; then
-    echo "[vim_setup] Installing pymode"
+    echo "${bold}[vim_setup]${normal} Installing pymode"
     pushd ~/.vim/bundle
-    echo "[vim_setup] Cloning python-mode repository"
+    echo "${bold}[vim_setup]${normal} Cloning python-mode repository"
     git clone https://github.com/klen/python-mode.git
     popd
-    echo "[vim_setup] Completed installing pymode"
+    echo "${bold}[vim_setup]${normal} Completed installing pymode"
 else
-    echo "[vim_setup] Skipping installing pymode -- already installed"
+    echo "${bold}[vim_setup]${normal} Skipping installing pymode -- already installed"
 fi
 
 # install the predawn theme
 if [ ! -f ~/.vim/colors/predawn.vim ]; then
-    echo "[vim_setup] Installing predawn theme"
-    echo "[vim_setup] Building colors directory"
+    echo "${bold}[vim_setup]${normal} Installing predawn theme"
+    echo "${bold}[vim_setup]${normal} Building colors directory"
     mkdir -p ~/.vim/colors
-    echo "[vim_setup] Downloading predawn theme"
+    echo "${bold}[vim_setup]${normal} Downloading predawn theme"
     curl -LSso ~/.vim/colors/predawn.vim https://raw.githubusercontent.com/juanedi/predawn.vim/master/colors/predawn.vim
-    echo "[vim_setup] Completed installing predawn theme"
+    echo "${bold}[vim_setup]${normal} Completed installing predawn theme"
 else
-    echo "[vim_setup] Skipping installing predawn theme -- already installed"
+    echo "${bold}[vim_setup]${normal} Skipping installing predawn theme -- already installed"
 fi
 # copy the rc
-echo "[vim_setup] Copying the vimrc"
+echo "${bold}[vim_setup]${normal} Copying the vimrc"
 cp ./.vimrc ~/.vimrc
 
-echo "[vim_setup] Vim setup complete"
+echo "${bold}[vim_setup]${normal} Vim setup complete"
